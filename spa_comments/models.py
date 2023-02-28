a55fdbf0de7b2a712abbf38b033dc52ea13b9d4e
Oleksii Bodnarchuk <olbody12@gmail.com>
2023-02-28 21:02:02 +0200

    added parent_coment field for nested comments

--- spa_comments/models.py
+++ spa_comments/models.py
@@ -6,6 +6,10 @@ class Comment(models.Model):
     email = models.EmailField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )
 
     def __str__(self):
         return f"{self.username} - {self.message}"
